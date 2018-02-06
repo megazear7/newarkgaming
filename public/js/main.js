1c32dd890dacb457f77d18f0bc73dbb23f11ebfd
alexlockhart7 <alex.lockhart@icfolson.com>
2018-02-06 14:48:53 -0500

    Loading images before displaying them

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,22 @@
-console.log("main.js loaded")
+document.querySelectorAll('img').forEach((img) => {
+  console.log("A");
+  img.addEventListener('load', () => {
+    console.log("B");
+    img.style.opacity = '1';
+  });
+});
+
+var backgroundImage = new Image()
+var backgroundImageUrl = '/images/gaming/playing1.jpg';
+
+backgroundImage.onload = () => {
+  var html = document.querySelector('html');
+  var overlayColor = document.querySelector('.overlay-color');
+  var container = document.querySelector('.container');
+  html.style.background = 'url('+backgroundImageUrl+') no-repeat center center fixed';
+  overlayColor.style.zIndex = '-1';
+  overlayColor.style.backgroundColor = '#4ABDACAA';
+  container.style.opacity = '1';
+}
+
+backgroundImage.src = backgroundImageUrl;
