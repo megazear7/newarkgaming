3ab58299751a96424af991cab86bde751ef3a9e2
alexlockhart7 <alex.lockhart@icfolson.com>
2018-02-07 15:51:33 -0500

    Cleaned up the tngg card component so that it is only concerned with the things it should be concerned about.

--- public/js/tngg-card.js
+++ public/js/tngg-card.js
@@ -14,9 +14,6 @@ export default class TnggCard extends HTMLElement {
   get bold() { return this._bold; }
   set bold(v) { this._bold = v; this.render() }
 
-  get noImage() { return this._noImage; }
-  set noImage(v) { this._noImage = v; this.render() }
-
   constructor() {
     super();
     this.attachShadow({mode: "open"});
@@ -33,27 +30,31 @@ export default class TnggCard extends HTMLElement {
     render(html`
       ${this._styles()}
       <div class="card ${this.bold ? 'bold' : ''} ${this.image ? '' : 'no-image'}">
-        ${this.banner ? "" : this._top()}
+        ${this._top()}
         ${this._bottom()}
       </div>
     `, this.shadowRoot);
   }
 
   _top() {
-    return html`
-      <div class="card-top">
-        <div class="card-header">
-          <slot name="title">NEED TITLE</slot>
+    if (this.banner) {
+      return '';
+    } else {
+      return html`
+        <div class="card-top">
+          <div class="card-header">
+            <slot name="title"></slot>
+          </div>
+          ${this.image ? html`<img src=${this.image}>` : ''}
         </div>
-        ${this.image ? html`<img src=${this.image}>` : ''}
-      </div>
-    `;
+      `;
+    }
   }
 
   _bottom() {
     return html`
       <div class="card-bottom">
-        <slot name="message">NEED MESSAGE</slot>
+        <slot name="message"></slot>
       </div>
     `;
   }
@@ -68,16 +69,19 @@ export default class TnggCard extends HTMLElement {
 
   _styles() {
     return html`<style>
+      :host {
+        display: block;
+      }
+
       .card {
         background-color: #fff;
-        border-radius: 2px;
+        border-radius: 3px;
         box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
-        margin-bottom: 0.5rem;
         height: 100%;
+        overflow: hidden;
       }
 
       .card-top {
-        border-radius: 2px 2px 0 0;
         position: relative;
         overflow: hidden;
         border-bottom: 2px solid #F78733;
@@ -85,27 +89,20 @@ export default class TnggCard extends HTMLElement {
       }
 
       .card-top .card-header {
-        box-sizing: border-box;
-        z-index: 2;
         position: relative;
-        width: 100%;
-        padding: 1rem 1rem;
-        font-size: 1.5rem;
+        z-index: 2;
+        padding: 1rem;
         color: #fff;
         text-shadow: 3px 3px 2px #000;
-        background-color: #FC4A1499;
         background-color: #7e798699;
       }
 
       .card-top img {
         position: absolute;
-        z-index: 1;
         top: 0;
         opacity: 0;
         transition: opacity 1s;
         width: 100%;
-        vertical-align: bottom;
-        border-radius: 2px 0;
       }
 
       .card-bottom {
@@ -119,33 +116,7 @@ export default class TnggCard extends HTMLElement {
       .card.bold .card-bottom {
         color: #fff;
         background-color: #FC4A14;
-        border-radius: 0 0 2px 2px;
         text-shadow: 2px 2px 2px #111d;
-      }
-
-      @supports (display: grid) {
-        @media (min-width: 600px) {
-          .card-top .card-header {
-            font-size: 2.25rem;
-          }
-        }
-
-        @media (min-width: 800px) {
-          .card {
-            margin-bottom: 0;
-            border-top: none;
-          }
-
-          .card-top .card-header {
-            font-size: 1.75rem;
-          }
-        }
-
-        @media (min-width: 1000px) {
-          .card-top .card-header {
-            font-size: 1.75rem;
-          }
-        }
       }
     </style>`;
   }
