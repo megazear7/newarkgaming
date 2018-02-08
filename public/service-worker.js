c5b19d984f429fec842f7aa6535432a46dbe72a7
alexlockhart7 <alex.lockhart@icfolson.com>
2018-02-08 13:11:17 -0500

    Pre caching the root page

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,10 @@
-const PRECACHE = 'precache-v2';
-const RUNTIME = 'runtime-v2';
+const PRECACHE = 'precache-v3';
+const RUNTIME = 'runtime-v3';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
+  "",
+  "/",
 ];
 
 // The install handler takes care of precaching the resources we always need.
