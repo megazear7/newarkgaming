25aa0cbcc5620fad1bcc4b5da654ce3378b2afa1
alexlockhart7 <alex.lockhart@icfolson.com>
2018-02-15 11:30:29 -0500

    Bumped service worker cache versions

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const PRECACHE = 'precache-v15';
-const RUNTIME = 'runtime-v15';
+const PRECACHE = 'precache-v16';
+const RUNTIME = 'runtime-v16';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
